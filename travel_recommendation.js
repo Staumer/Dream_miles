14f1e3f4863d1ffb02fdb1ed1ba9347685b76a46
Staumer <staumerstaxidermy@gmail.com>
2026-06-23 11:40:20 -0500

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,6 +4,35 @@ document.getElementById("year").textContent = new Date().getFullYear();
 // Example: Show an alert when the recommendation button is clicked
 const button = document.querySelector("button");
 
+function searchCountries() {
+    const input = document.getElementById('countriesInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('travel_recommendation_api.json')
+      .then(response => response.json())
+      .then(data => {
+        const countries = data.countries.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const name = countries.name.join(', ');
+          const description = countries.description.join(', ');
+        
+
+          resultDiv.innerHTML += `<h2>${countries.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${countries.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Name:</strong> ${name}</p>`;
+          resultDiv.innerHTML += `<p><strong>Description:</strong> ${description}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Country not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
 if (button) {
   button.addEventListener("click", () => {
     alert("Your Dream Miles recommendations are being imagined!");
